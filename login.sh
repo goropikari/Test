e18153db264259acca0d4ce6d204ddce8ddd3d50
goropikari <goropikari56@gmail.com>
2024-04-14 14:55:48 +0900

    self devcontainer feature

--- login.sh
+++ login.sh
@@ -4,5 +4,5 @@
 CMD=${1:-bash}
 
 docker exec -it -u vscode \
-		-e SSH_AUTH_SOCK=/home/vscode/.ssh-auth-sock \
-		test_devcontainer-app-1 $CMD
+	-e SSH_AUTH_SOCK=/home/vscode/.ssh-auth-sock \
+	test_devcontainer-app-1 $CMD
